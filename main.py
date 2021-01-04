b971abe77ee4c17ec2c7a0c3ba8e5892de9112b7
jesuisjohan <thomas.maihoanganhvu@gmail.com>
2021-01-04 19:52:20 +0700

    Add equation 1 to 5

--- main.py
+++ main.py
@@ -1,16 +1,37 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+8 to toggle the breakpoint.
+# 1
+def cap_CO2_Air_CO2_dot_air(mc_blow_air, mc_ext_air, mc_pad_air, mc_air_can, mc_air_top, mc_air_out):
+    return mc_blow_air + mc_ext_air + mc_pad_air - mc_air_can - mc_air_top - mc_air_out
 
 
-# Press the green button in the gutter to run the script.
+# 2
+def cap_CO2_Top_CO2_dot_top(mc_air_top, mc_top_out):
+    return mc_air_top - mc_top_out
+
+
+# 3
+def MC_BlowAir(eta_heat_co2, u_blow, p_blow, a_flr):
+    return eta_heat_co2 * u_blow * p_blow / a_flr
+
+
+# 4
+def MC_ExtAir(u_ext_co2, phi_ext_co2, a_flr):
+    return u_ext_co2 * phi_ext_co2 / a_flr
+
+
+# 5
+
+def f_Pad(u_pad, phi_pad, a_flr):
+    return u_pad * phi_pad / a_flr
+
+
+def MC_PadAir(u_pad, phi_pad, a_flr, co2_out, co2_air):
+    f_pad = f_Pad(u_pad, phi_pad, a_flr)
+    return f_pad * (co2_out - co2_air)
+
+
+# if need to overload the above function, use this
+
+# 6
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    print('Hello world')
